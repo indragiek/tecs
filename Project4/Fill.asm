7296bafbcdb8ebe1425453a7ae23ca30ea84587b
Indragie Karunaratne <i@indragie.com>
2015-01-06 23:22:43 -0700

    Fix formatting

--- Project4/Fill.asm
+++ Project4/Fill.asm
@@ -8,38 +8,38 @@
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
 
-		@8192	// (512 * 32) / 16
-		D=A
-		@count
-		M=D		// count = 8192 (# of bytes)
+        @8192   // (512 * 32) / 16
+        D=A
+        @count
+        M=D     // count = 8192 (# of bytes)
 (LOOP)
-		@index
-		M=0 	// index = 0
+        @index
+        M=0     // index = 0
 (INNER)
-		@KBD
-		D=M
-		@WHITE
-		D;JEQ	// goto WHITE if KBD value is 0
+        @KBD
+        D=M
+        @WHITE
+        D;JEQ   // goto WHITE if KBD value is 0
 (BLACK)
-		@index
-		D=M
-		@SCREEN
-		A=A+D 	// Calculate byte address
-		M=-1 	// Fill with black
-		@END
-		0;JMP	// goto END
+        @index
+        D=M
+        @SCREEN
+        A=A+D   // Calculate byte address
+        M=-1    // Fill with black
+        @END
+        0;JMP   // goto END
 (WHITE)
-		@index
-		D=M
-		@SCREEN
-		A=A+D 	// Calculate byte address
-		M=0		// Fill with white
-(END)	
-		@index
-		MD=M+1 	// Increment index by 1
-		@count
-		D=D-M
-		@LOOP
-		D;JEQ	// goto LOOP if count - index == 0
-		@INNER
-		0;JMP	// goto INNER
+        @index
+        D=M
+        @SCREEN
+        A=A+D   // Calculate byte address
+        M=0     // Fill with white
+(END)   
+        @index
+        MD=M+1  // Increment index by 1
+        @count
+        D=D-M
+        @LOOP
+        D;JEQ   // goto LOOP if count - index == 0
+        @INNER
+        0;JMP   // goto INNER
--- Project4/Mult.asm
+++ Project4/Mult.asm
@@ -6,27 +6,27 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-		@sum	
-		M=0 	// Initialize sum to 0
-		@R1
-		D=M
-		@count	
-		M=D 	// Initialize count to the value at RAM[1]
-(LOOP)	
-		@count
-		D=M
-		@END
-		D;JEQ 	// Check if count is 0, if so, goto END
-		@R0
-		D=M
-		@sum
-		M=M+D	// Add RAM[0] to sum
-		@count
-		M=M-1	// Decrement count by 1
-		@LOOP
-		0;JMP	// Loop again
-(END)	
-		@sum
-		D=M
-		@R2
-		M=D 	// Write sum to RAM[2]
+        @sum    
+        M=0     // Initialize sum to 0
+        @R1
+        D=M
+        @count  
+        M=D     // Initialize count to the value at RAM[1]
+(LOOP)  
+        @count
+        D=M
+        @END
+        D;JEQ   // Check if count is 0, if so, goto END
+        @R0
+        D=M
+        @sum
+        M=M+D   // Add RAM[0] to sum
+        @count
+        M=M-1   // Decrement count by 1
+        @LOOP
+        0;JMP   // Loop again
+(END)   
+        @sum
+        D=M
+        @R2
+        M=D     // Write sum to RAM[2]
